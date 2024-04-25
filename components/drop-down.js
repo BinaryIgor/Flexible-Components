4ca51bc30dd3ab3f09248059f2cfff9f4f855329
Igor <41533396+BinaryIgor@users.noreply.github.com>
2024-04-25 17:06:17 +0200

    Improve DropDown

--- components/drop-down.js
+++ components/drop-down.js
@@ -2,77 +2,112 @@ import { Components } from "./base.js";
 
 const hiddenClass = "hidden";
 
-const defaultContainerClass = "cursor-pointer text-xl w-fit";
-const defaultDropdownOptionsClass = "top-8 right-0 rounded-md w-full space-y-1";
-const defaultDropdownOptionClass = "bg-slate-100 py-2 px-4 text-right";
+const containerClassDefault = "whitespace-nowrap cursor-pointer text-xl w-fit";
+const titleClassDefault = "text-xl py-2";
+const optionsClassDefault = "top-8 right-0 w-full space-y-1 overflow-auto max-h-[90vh]";
+const optionClassDefault = "py-2 px-4 text-right rounded bg-slate-50";
+
+const attributes = {
+    hideOptionsOnOutsideClick: "hide-options-on-outside-click",
+    hideOptionsOnSelected: "hide-options-on-selected"
+};
+
+const optionDataId = "data-id";
+
+const elements = {
+    title: "title",
+    options: "options",
+    option: "option",
+    optionOfIdx(idx) {
+        return `option-${idx}`
+    }
+};
 
 const MAX_OPTIONS = 100;
 
 class DropDown extends HTMLElement {
 
+    set options(options) {
+        this._options = options;
+        const optionsHTML = this._optionItemsHTML();
+        this._optionsElement.innerHTML = optionsHTML;
+        this._setOptionItemsOnClick();
+    }
+
     connectedCallback() {
         const title = Components.attributeValueOrDefault(this, "title", "Drop Down");
+        const titleAttributes = Components.mappedAttributes(this, elements.title, { defaultClass: titleClassDefault });
 
-        this._hideOptionsOnSelected = Components.attributeValueOrDefault(this, "hide-options-on-selected", true);
+        const optionsAttributes = Components.mappedAttributes(this, elements.options, {
+            defaultClass: optionsClassDefault,
+            toAddClass: hiddenClass
+        });
 
-        const optionItems = this._optionItemsFromAttributesOrProperty();
+        this._hideOptionsOnOutsideClick = Components.attributeBooleanValueOrDefault(this, attributes.hideOptionsOnOutsideClick, true);
+        this._hideOptionsOnSelected = Components.attributeBooleanValueOrDefault(this, attributes.hideOptionsOnSelected, true);
 
-        const optionsHTML = optionItems.join("\n");
+        const optionsHTML = this._optionItemsHTML();
 
-        //TODO: what classes are really needed?
         this.innerHTML = `
-        <div class="relative ${defaultContainerClass}">
-            <div>${title}</div>
-            <ul class="${hiddenClass} whitespace-nowrap absolute ${defaultDropdownOptionsClass}">
+        <div class="relative ${containerClassDefault}">
+            <div ${titleAttributes}>${title}</div>
+            <ul style="z-index: 999" ${optionsAttributes}>
                 ${optionsHTML}
             </ul>
         </div>
         `;
 
         const options = this.querySelector("ul");
+        this._optionsElement = options;
 
         const container = this.querySelector("div");
         container.onclick = (e) => {
-            options.classList.remove(hiddenClass);
+            // Do not hide other, opened DropDowns
+            e.stopPropagation();
+            options.classList.toggle(hiddenClass);
         };
 
-        this.hideOptions = (e) => {
+        this.hideOptions = () => {
             options.classList.add(hiddenClass);
         };
 
-        this.querySelectorAll("li").forEach(li => {
-            li.onclick = (e) => {
-                // For controlled hide to work
-                e.stopPropagation();
+        this._hideOptionsIfOutside = (e) => {
+            if (e.target != container && e.target.parentNode != container) {
+                options.classList.add(hiddenClass);
+            }
+        };
 
-                if (this._hideOptionsOnSelected == true) {
-                    options.classList.add(hiddenClass);
-                }
+        this._setOptionItemsOnClick();
 
-                console.log("Option chosen!", li);
-                this.dispatchEvent(new CustomEvent("drop-down-option-chosen", {
-                    detail: { text: li.textContent, dataId: li.getAttribute("data-id") }
-                }));
-            };
-        });
+        if (this._hideOptionsOnOutsideClick == true) {
+            window.addEventListener("click", this._hideOptionsIfOutside);
+        }
     }
 
-    _optionItemsFromAttributesOrProperty() {
-        if (this.options) {
-            return this._optionItemsFromPropery(this.options);
+    _optionItemsHTML() {
+        let optionItems;
+        if (this._options) {
+            optionItems = this._optionItemsFromPropery(this._options);
+        } else {
+            optionItems = this._optionItemsFromAttributes();
         }
-        return this._optionItemsFromAttributes();
+        return optionItems.join("\n");
     }
 
     _optionItemsFromPropery(options) {
         const optionItems = [];
 
-        for (let o of options) {
-            const optionAttributes = Components.mappedAttributes(this, "option", {
+        const optionDefaultAttributes = Components.mappedAttributesAsObject(this, elements.option, {
+            defaultClass: optionClassDefault
+        });
+
+        for (let i = 0; i < options.length; i++) {
+            const o = options[i];
+            const optionAttributes = Components.mappedAttributes(this, elements.optionOfIdx(i), {
                 defaultAttributes: {
-                    "data-id": o.id
-                },
-                defaultClass: defaultDropdownOptionClass
+                    ...optionDefaultAttributes,
+                    optionDataId: o.id
+                }
             });
             optionItems.push(this._optionItemHtml(optionAttributes, o.title));
         }
@@ -87,19 +122,49 @@ class DropDown extends HTMLElement {
     _optionItemsFromAttributes() {
         const optionItems = [];
 
+        const optionDefaultAttributes = Components.mappedAttributesAsObject(this, elements.option, {
+            defaultClass: optionClassDefault
+        });
+
         for (let i = 0; i < MAX_OPTIONS; i++) {
-            const optionId = `option-${i}`;
+            const optionId = elements.optionOfIdx(i);
             const optionTitle = this.getAttribute(`${optionId}-text`);
             if (!optionTitle) {
                 break;
             }
             const optionAttributes = Components.mappedAttributes(this, optionId, {
-                defaultClass: defaultDropdownOptionClass
+                defaultAttributes: optionDefaultAttributes
             });
             optionItems.push(this._optionItemHtml(optionAttributes, optionTitle));
         }
 
         return optionItems;
+    }
+
+    _setOptionItemsOnClick() {
+        this.querySelectorAll("li").forEach(li => {
+            li.onclick = (e) => {
+                // For controlled hide to work
+                e.stopPropagation();
+
+                if (this._hideOptionsOnSelected) {
+                    this.hideOptions();
+                }
+
+                this.dispatchEvent(new CustomEvent("drop-down-option-chosen", {
+                    detail: {
+                        text: li.textContent,
+                        id: li.getAttribute(optionDataId)
+                    }
+                }));
+            };
+        });
+    }
+
+    disconnectedCallback() {
+        if (this._hideOptionsOnOutsideClick == true) {
+            window.removeEventListener("click", this._hideOptionsIfOutside);
+        }
     }
 }
 
--- server/components/drop-down.js
+++ server/components/drop-down.js
@@ -7,26 +7,71 @@ export const PATH = "/drop-down";
 export const router = express.Router();
 
 router.get("/", (req, res) => {
+    const manyOptions = [];
+    for (let i = 0; i < 100; i++) {
+        manyOptions.push(`option-${i}-text="Right Option ${i+1}"`);
+    }
+    const manyOptionsTexts = manyOptions.join("\n");
+
     const body = `
-        <drop-down title="Some Custom Title"
-            id="drop-down"
+        <drop-down title="HTMX & attributes-based Drop-down"
+            hide-options-on-outside-click="false"
+            hide-options-on-selected="false"
+            id="attributes-drop-down"
+            option:add:class="text-2xl"
             option-0-text="A"
+            option-0:add:class="font-bold"
             option-0:data-id="A-1"
             option-0:hx-get="${PATH}/option-0"
             option-0:hx-push-url="true"
-            option-0:hx-target="#drop-down"
+            option-0:hx-target="#attributes-drop-down"
             option-1-text="B"
             option-1:data-id="B-1">
+        </drop-down>
+
+        <drop-down title="JS-based Drop-down"
+            id="js-drop-down">
+        </drop-down>
+
+        <drop-down title="Right Drop-down with many options" class="absolute top-0 right-0 z-10"
+            title:class="font-bold text-xl text-slate-800 p-4"
+            option-0:class="bg-slate-300 text-right p-2 text-slate-600"
+            ${manyOptionsTexts}>
         </drop-down>
     `;
 
     const script = `
-        console.log("Some script...");
+        const attributesDropdown = document.getElementById("attributes-drop-down");
 
-        const dropdown = document.querySelector("drop-down");
+        attributesDropdown.addEventListener("drop-down-option-chosen", e => {
+            console.log("Option chosen for HTMX & attributes dropdown...", e.detail);
+            // dropdown.hideOptions();
+        });
 
-        dropdown.addEventListener("drop-down-option-chosen", e => {
-            console.log("Option chosen...", e.detail);
+        const jsDropdown = document.getElementById("js-drop-down");
+        
+        // On purpose to test out after component initialization property set
+        setTimeout(() => {
+            console.log("Late set...");
+
+            jsDropdown.setAttribute("option:add:class", "italic");
+            jsDropdown.setAttribute("option-0:add:class", "text-2xl");
+
+            const options = [
+                {
+                    title: "Option A",
+                    id: 22
+                },
+                {
+                    title: "Option B"
+                }
+            ];
+    
+            jsDropdown.options = options;
+        }, 500);
+
+        jsDropdown.addEventListener("drop-down-option-chosen", e => {
+            console.log("Option chosen for js dropdown...", e.detail);
             // dropdown.hideOptions();
         });
     `;
