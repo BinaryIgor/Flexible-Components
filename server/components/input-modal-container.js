48db2e2b687591cff6cb41134463fd74b9a60db4
Igor <41533396+BinaryIgor@users.noreply.github.com>
2024-05-05 21:01:21 +0200

    Working on input modal

--- server/components/input-modal-container.js
+++ server/components/input-modal-container.js
@@ -8,13 +8,14 @@ export const router = express.Router();
 
 router.get("/", (req, res) => {
     const body = `
-    <input-modal-container title="Some Food Item" ok-text="Ok">
+    <input-modal-container title="Some Food Item" ok-text="Ok" 
+        hx-post="${PATH}/food-items" hx-trigger="add-food-item-trigger" hx-include="this">
       <input-with-error 
         container:class="mx-4"
         input:type="text"
-        input:name="kcal"
+        input:name="kcals"
         input:placeholder="Input some calories..."
-        input:hx-post="${PATH}/validate-kcal"
+        input:hx-post="${PATH}/food-items/validate-kcals"
         input:hx-trigger="input changed delay:500ms"
         input:hx-swap="outerHTML"
         input:hx-target="next input-error">
@@ -24,7 +25,7 @@ router.get("/", (req, res) => {
         input:type="text"
         input:name="protein"
         input:placeholder="Input some protein..."
-        input:hx-post="${PATH}/validate-protein"
+        input:hx-post="${PATH}/food-items/validate-protein"
         input:hx-trigger="input changed delay:500ms"
         input:hx-swap="outerHTML"
         input:hx-target="next input-error">
@@ -40,10 +41,16 @@ router.get("/", (req, res) => {
         const showInputModalButton = document.getElementById("show-input-modal");
         const inputModal = document.querySelector("input-modal-container");
 
+        const [ kcalsInput, proteinInput] = document.querySelectorAll("input-with-error");
+
+        console.log(kcalsInput, proteinInput);
+
         showInputModalButton.onclick = () => {
             inputModal.onOk = () => {
                 console.log("Ok clicked!");
-                inputModal.hide();
+                // inputModal.hide();
+                document.dispatchEvent(new Event("add-food-item-trigger"));
+                inputModal.dispatchEvent(new Event("add-food-item-trigger"));
             };
             inputModal.show();
         };
@@ -51,11 +58,57 @@ router.get("/", (req, res) => {
         window.addEventListener("input-modal-container-hidden", e => {
             console.log("Input modal container was hidden!");
         });
+
+        inputModal.addEventListener("htmx:afterRequest", e => {
+            console.log("Input modal, after request...", e.detail);
+            if (e.detail.failed) {
+                const response = e.detail.xhr.responseText;
+                const [kcalsError, proteinError] = response.split('||');
+                console.log("Error response:", kcalsError, proteinError);
+                kcalsInput.onInputValidated(kcalsError);
+                proteinInput.onInputValidated(proteinError);
+            }
+        });
     `;
 
     Web.returnHtml(res, Web.htmlPage(body, NAME, script, ["input-error", "input-with-error"]));
 });
 
-router.post(`/validate-kcal`, (req, res) => {
-    Web.returnHtml(res, `<input-error message="Kcals are not valid!"></input-error>`);
+router.post(`/food-items/validate-kcals`, (req, res) => {
+    Web.returnHtml(res, `<input-error message="${validateKcals(req.body.kcals)}"></input-error>`);
+});
+
+function validateKcals(kcals) {
+    return isPositiveNumber(kcals) ? "" : "Kcals need to be a positive number";
+}
+
+function isPositiveNumber(number) {
+    try {
+        return !isNaN(number) && parseInt(number) > 0;
+    } catch (e) {
+        return false;
+    }
+}
+
+router.post(`/food-items/validate-protein`, (req, res) => {
+    Web.returnHtml(res, `<input-error message="${validatProtein(req.body.protein)}"></input-error>`);
+});
+
+function validatProtein(protein) {
+    return isPositiveNumber(protein) ? "" : "Protein need to be a positive number";
+}
+
+router.post("/food-items", (req, res) => {
+    const kcals = req.body.kcals;
+    const protein = req.body.protein;
+
+    const kcalsError = validateKcals(kcals);
+    const proteinError = validatProtein(protein);
+    if (!kcalsError && !proteinError) {
+        // TODO: do add food item
+        console.log("Adding food item...");
+        Web.returnText(res, "OK", 201);
+    } else {
+        Web.returnText(res,`${kcalsError}||${proteinError}`, 400);
+    }
 });
